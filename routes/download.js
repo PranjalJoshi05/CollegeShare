0f7ca1b6c4afc74af3fd5268e84927c4c5d6fa32
pranjal-joshi-pj <pranjal.ksp@gmail.com>
2021-06-25 19:07:20 +0530

    Fix search filter

--- routes/download.js
+++ routes/download.js
@@ -1,22 +1,31 @@
 const router = require('express').Router();
 const File = require('../models/file');
 
+//find distinct subjects
+let subjects;
+File.find().distinct( 'subject', (error,items)=>{
+  subjects = items;
+});
+
+//file download route
 router.get('/api/:filename', async (req, res) => {
   const file = await File.findOne({ filename: req.params.filename});
   const filePath = `${__dirname}/../${file.path}`;
   res.download(filePath);
 });
 
+//download route
 router.get('/',(req,res)=>{
   File.find(req.query,(err, foundItems)=>{
     if(err){
       console.log(err);
     }else{
-      res.render('download', {files:foundItems});
+      res.render('download', {files: foundItems, filters: req.query, subjects: subjects});
     }
   });
 });
 
+//filter search
 router.post('/filter',(req,res)=>{
   const subject = req.body.subject;
   const category = req.body.category;
